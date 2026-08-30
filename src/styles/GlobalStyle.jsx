f02b0085c357ff2bbc16b3f7a11465a3b54d0160
LeeJooyeon <smilejuju@ewha.ac.kr>
2026-08-30 21:32:31 +0900

    design. globalstyle 폰트 수정

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,45 +1,51 @@
+import SuitLight from "../assets/fonts/SUIT-Light.woff2";
+import SuitRegular from "../assets/fonts/SUIT-Regular.woff2";
+import SuitMedium from "../assets/fonts/SUIT-Medium.woff2";
+import SuitSemiBold from "../assets/fonts/SUIT-SemiBold.woff2";
+import SuitBold from "../assets/fonts/SUIT-Bold.woff2";
+import SuitExtraBold from "../assets/fonts/SUIT-ExtraBold.woff2";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
   @font-face {
     font-family: "SUIT";
-    src: url("../assets/fonts/SUIT-Light.woff2") format("woff2");
+    src: url(${SuitLight}) format("woff2");
     font-weight: 300;
     font-style: normal;
   }
 
   @font-face {
     font-family: "SUIT";
-    src: url("../assets/fonts/SUIT-Regular.woff2") format("woff2");
+    src: url(${SuitRegular}) format("woff2");
     font-weight: 400;
     font-style: normal;
   }
 
   @font-face {
     font-family: "SUIT";
-    src: url("../assets/fonts/SUIT-Medium.woff2") format("woff2");
+    src: url(${SuitMedium}) format("woff2");
     font-weight: 500;
     font-style: normal;
   }
 
   @font-face {
     font-family: "SUIT";
-    src: url("../assets/fonts/SUIT-SemiBold.woff2") format("woff2");
+    src: url(${SuitSemiBold}) format("woff2");
     font-weight: 600;
     font-style: normal;
   }
 
   @font-face {
     font-family: "SUIT";
-    src: url("../assets/fonts/SUIT-Bold.woff2") format("woff2");
+    src: url(${SuitBold}) format("woff2");
     font-weight: 700;
     font-style: normal;
   }
 
   @font-face {
     font-family: "SUIT";
-    src: url("../assets/fonts/SUIT-ExtraBold.woff2") format("woff2");
+    src: url(${SuitExtraBold}) format("woff2");
     font-weight: 800;
     font-style: normal;
   }
